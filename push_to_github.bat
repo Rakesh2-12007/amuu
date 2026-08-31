b5119de4bfb510a36018ac02f9cd1e1fd7e96216
Rakesh S <rakesh@users.noreply.github.com>
2026-08-31 19:36:52 +0530

    Add push_to_github helper script

---
+++ push_to_github.bat
@@ -0,0 +1,15 @@
+@echo off
+title Pushing Amuu Website to GitHub
+echo ==========================================================
+echo 💖 PUSHING AMUU ROMANTIC WEBSITE TO GITHUB 💖
+echo ==========================================================
+echo.
+echo Preparing to push to: https://github.com/Rakesh2-12007/amuu.git
+echo.
+echo A browser window/popup will open to authenticate.
+echo Please sign in with your 'Rakesh2-12007' account when prompted.
+echo.
+git push https://github.com/Rakesh2-12007/amuu.git main
+echo.
+echo Done! You can close this window.
+pause
